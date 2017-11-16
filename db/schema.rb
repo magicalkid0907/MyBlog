ff82c8b5e8d123bb4747da6adb53c8bea69f5e10
Tomoya Baba <magicalkid0907@gmail.com>
2017-11-16 09:48:20 +0000

    Add name

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171115050114) do
+ActiveRecord::Schema.define(version: 20171116094054) do
 
   create_table "messages", force: :cascade do |t|
     t.string "title"
     t.string "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
   end
 
 end
